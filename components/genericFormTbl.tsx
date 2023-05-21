37fbc1705961933a888b0387bf6e19a2a4d9681c
sashin <sashin.lutchan@xetgroup.com>
2023-05-21 20:09:49 +0200

    update to generic from tbl

--- components/genericFormTbl.tsx
+++ components/genericFormTbl.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent, MouseEvent } from "react";
 import { useState } from "react";
 
 interface Items {
@@ -7,6 +7,7 @@ interface Items {
   UnitPrice: number;
   Total: number;
 }
+
 type PopupProps = {
   onClose: (data: Items) => void;
 };
@@ -20,33 +21,29 @@ interface FormData {
 
 export default function GenericFormTbl({ onClose }: PopupProps) {
   const [popup, setPopup] = useState(false);
-  const [formData, setFormdata] = useState<FormData>({
+  const [formData, setFormData] = useState<FormData>({
     Description: "",
     Qty: 0,
     UnitPrice: 0,
     Total: 0,
   });
-  const [errormessage, setErrorMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
 
   function calculateTotal() {
     let value = formData.Qty * formData.UnitPrice;
-    setFormdata({ ...formData, Total: value });
+    setFormData((formData) => ({ ...formData, Total: value }));
   }
+
   const toggleModal = () => {
     setPopup(!popup);
   };
 
-  const handleSubmit = (event: React.MouseEvent<HTMLInputElement>) => {
-  
+  const handleSubmit = (event: MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     if (
-      formData.Description === null ||
       formData.Description === "" ||
-      formData.Qty === null ||
       formData.Qty === 0 ||
-      formData.Total === null ||
       formData.Total === 0 ||
-      formData.UnitPrice === null ||
       formData.UnitPrice === 0
     ) {
       setErrorMessage(true);
@@ -54,13 +51,12 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
       setErrorMessage(false);
       const data: Items = { ...formData };
       onClose(data);
-      setFormdata({
+      setFormData({
         Description: "",
         Qty: 0,
         UnitPrice: 0,
         Total: 0,
       });
-
       toggleModal();
     }
   };
@@ -69,7 +65,7 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
     <>
       <br />
       <button
-        className=" relative left-64 py-2 px-6 inline-flex justify-center  border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+        className="relative left-64 py-2 px-6 inline-flex justify-center border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-blue-500 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
         onClick={toggleModal}
       >
         Add Item
@@ -77,33 +73,20 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
 
       <br />
       {popup && (
-        <div
-          className="fixed z-10 overflow-y-auto top-0 w-full left-0"
-          id="modal"
-        >
+        <div className="fixed z-10 overflow-y-auto top-0 w-full left-0" id="modal">
           <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
             <div className="fixed inset-0 transition-opacity">
               <div className="absolute inset-0 bg-gray-900 opacity-75" />
             </div>
-            <span className="hidden sm:inline-block sm:align-middle sm:h-screen">
-              &#8203;
-            </span>
+            <span className="hidden sm:inline-block sm:align-middle sm:h-screen">&#8203;</span>
 
-            <div
-              className="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full"
-              role="dialog"
-              aria-modal="true"
-              aria-labelledby="modal-headline"
-            >
-              {/* <span>  { errormessage === true ? (
+            <div className="inline-block align-center bg-white rounded-lg text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+              {/* <span>  { errorMessage === true ? (
                 <>Please Ensure All Fields are filled</>
               ):(<></>)}</span> */}
 
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="name"
-                >
+                <label className="block text-sm font-medium text-gray-700" htmlFor="name">
                   Description
                 </label>
                 <input
@@ -111,22 +94,16 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
                   id="Description"
                   name="Description"
                   value={formData.Description}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
                     const { name, value } = event.target;
-                    setFormdata((formData) => ({
-                      ...formData,
-                      [name]: value,
-                    }));
+                    setFormData((formData) => ({ ...formData, [name]: value }));
                   }}
                   className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
                 />
               </div>
 
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="name"
-                >
+                <label className="block text-sm font-medium text-gray-700" htmlFor="name">
                   Qty
                 </label>
                 <input
@@ -134,21 +111,16 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
                   id="Qty"
                   name="Qty"
                   value={formData.Qty}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
                     const { name, value } = event.target;
-                    setFormdata((formData) => ({
-                      ...formData,
-                      [name]: value,
-                    }));
+                    setFormData((formData) => ({ ...formData, [name]: parseInt(value) }));
                   }}
                   className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
                 />
               </div>
+
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="UnitPrice"
-                >
+                <label className="block text-sm font-medium text-gray-700" htmlFor="UnitPrice">
                   UnitPrice
                 </label>
                 <input
@@ -156,12 +128,9 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
                   id="UnitPrice"
                   name="UnitPrice"
                   value={formData.UnitPrice}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
                     const { name, value } = event.target;
-                    setFormdata((formData) => ({
-                      ...formData,
-                      [name]: parseFloat(value), // Convert the value to a number using parseFloat
-                    }));
+                    setFormData((formData) => ({ ...formData, [name]: parseFloat(value) }));
                   }}
                   onBlur={calculateTotal}
                   className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
@@ -169,10 +138,7 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
               </div>
 
               <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-                <label
-                  className="block text-sm font-medium text-gray-700"
-                  htmlFor="name"
-                >
+                <label className="block text-sm font-medium text-gray-700" htmlFor="name">
                   Total
                 </label>
                 <input
@@ -180,14 +146,12 @@ export default function GenericFormTbl({ onClose }: PopupProps) {
                   id="Total"
                   name="Total"
                   value={formData.Total}
-                  onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                  onChange={(event: ChangeEvent<HTMLInputElement>) => {
                     const { name, value } = event.target;
-                    setFormdata((formData) => ({
-                      ...formData,
-                      [name]: value,
-                    }));
+                    setFormData((formData) => ({ ...formData, [name]: value }));
                   }}
-                  readOnly className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
+                  readOnly
+                  className="mt-1 py-2 px-3 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border border-gray-300 rounded-md"
                 />
               </div>
 
